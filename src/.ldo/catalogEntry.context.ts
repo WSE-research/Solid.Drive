e6f5530675bdb295b50a6173169a75790c2726c0
Parnian Hajian <parnian.hajian@stud.htwk-leipzig.de>
2026-03-12 19:18:43 +0100

    feat: replace PostSh with CatalogEntry ShEx shape including dcterms:conformsTo

--- src/.ldo/catalogEntry.context.ts
+++ src/.ldo/catalogEntry.context.ts
@@ -2,10 +2,10 @@ import type { LdoJsonldContext } from "@ldo/ldo";
 
 /**
  * =============================================================================
- * postContext: JSONLD Context for post
+ * catalogEntryContext: JSONLD Context for catalogEntry
  * =============================================================================
  */
-export const postContext: LdoJsonldContext = {
+export const catalogEntryContext: LdoJsonldContext = {
   type: {
     "@id": "@type",
     "@isCollection": true,
@@ -54,10 +54,14 @@ export const postContext: LdoJsonldContext = {
         "@id": "http://schema.org/publisher",
         "@type": "@id",
       },
+      conformsTo: {
+        "@id": "http://purl.org/dc/terms/conformsTo",
+        "@type": "@id",
+      },
     },
   },
-  CreativeWork: {
-    "@id": "http://schema.org/CreativeWork",
+  ImageFile: {
+    "@id": "https://example.com/app#ImageFile",
     "@context": {
       type: {
         "@id": "@type",
@@ -100,10 +104,14 @@ export const postContext: LdoJsonldContext = {
         "@id": "http://schema.org/publisher",
         "@type": "@id",
       },
+      conformsTo: {
+        "@id": "http://purl.org/dc/terms/conformsTo",
+        "@type": "@id",
+      },
     },
   },
-  Thing: {
-    "@id": "http://schema.org/Thing",
+  VideoFile: {
+    "@id": "https://example.com/app#VideoFile",
     "@context": {
       type: {
         "@id": "@type",
@@ -144,6 +152,110 @@ export const postContext: LdoJsonldContext = {
       },
       publisher: {
         "@id": "http://schema.org/publisher",
+        "@type": "@id",
+      },
+      conformsTo: {
+        "@id": "http://purl.org/dc/terms/conformsTo",
+        "@type": "@id",
+      },
+    },
+  },
+  AudioFile: {
+    "@id": "https://example.com/app#AudioFile",
+    "@context": {
+      type: {
+        "@id": "@type",
+        "@isCollection": true,
+      },
+      name: {
+        "@id": "http://schema.org/name",
+        "@type": "http://www.w3.org/2001/XMLSchema#string",
+      },
+      description: {
+        "@id": "http://schema.org/description",
+        "@type": "http://www.w3.org/2001/XMLSchema#string",
+      },
+      encodingFormat: {
+        "@id": "http://schema.org/encodingFormat",
+        "@type": "http://www.w3.org/2001/XMLSchema#string",
+      },
+      contentSize: {
+        "@id": "http://schema.org/contentSize",
+        "@type": "http://www.w3.org/2001/XMLSchema#string",
+      },
+      uploadDate: {
+        "@id": "http://schema.org/uploadDate",
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+      },
+      dateModified: {
+        "@id": "http://schema.org/dateModified",
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+      },
+      isPartOf: {
+        "@id": "http://schema.org/isPartOf",
+        "@type": "@id",
+      },
+      sharedWith: {
+        "@id": "http://schema.org/sharedWith",
+        "@type": "@id",
+        "@isCollection": true,
+      },
+      publisher: {
+        "@id": "http://schema.org/publisher",
+        "@type": "@id",
+      },
+      conformsTo: {
+        "@id": "http://purl.org/dc/terms/conformsTo",
+        "@type": "@id",
+      },
+    },
+  },
+  TextDocument: {
+    "@id": "https://example.com/app#TextDocument",
+    "@context": {
+      type: {
+        "@id": "@type",
+        "@isCollection": true,
+      },
+      name: {
+        "@id": "http://schema.org/name",
+        "@type": "http://www.w3.org/2001/XMLSchema#string",
+      },
+      description: {
+        "@id": "http://schema.org/description",
+        "@type": "http://www.w3.org/2001/XMLSchema#string",
+      },
+      encodingFormat: {
+        "@id": "http://schema.org/encodingFormat",
+        "@type": "http://www.w3.org/2001/XMLSchema#string",
+      },
+      contentSize: {
+        "@id": "http://schema.org/contentSize",
+        "@type": "http://www.w3.org/2001/XMLSchema#string",
+      },
+      uploadDate: {
+        "@id": "http://schema.org/uploadDate",
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+      },
+      dateModified: {
+        "@id": "http://schema.org/dateModified",
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+      },
+      isPartOf: {
+        "@id": "http://schema.org/isPartOf",
+        "@type": "@id",
+      },
+      sharedWith: {
+        "@id": "http://schema.org/sharedWith",
+        "@type": "@id",
+        "@isCollection": true,
+      },
+      publisher: {
+        "@id": "http://schema.org/publisher",
+        "@type": "@id",
+      },
+      conformsTo: {
+        "@id": "http://purl.org/dc/terms/conformsTo",
         "@type": "@id",
       },
     },
@@ -183,6 +295,10 @@ export const postContext: LdoJsonldContext = {
   },
   publisher: {
     "@id": "http://schema.org/publisher",
+    "@type": "@id",
+  },
+  conformsTo: {
+    "@id": "http://purl.org/dc/terms/conformsTo",
     "@type": "@id",
   },
 };
