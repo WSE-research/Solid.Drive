14b0543057ea608e14e51346d38616e733d4dc60
Parnian Hajian <parnian.hajian@stud.htwk-leipzig.de>
2026-03-18 15:02:26 +0100

    refactor: rename unclear variables

--- src/FileExplorer.tsx
+++ src/FileExplorer.tsx
@@ -52,6 +52,14 @@ export const FileExplorer: FunctionComponent<FileExplorerProps> = ({
   }, [session.webId]);
 
   useEffect(() => {
+    initialized.current = false;
+    setAppContainerUri(undefined);
+    setStorageRootUri("");
+    setCurrentUri(undefined);
+    setBreadcrumbs([]);
+  }, [session.webId]);
+
+  useEffect(() => {
     if (initialized.current) return;
     const storageRootId = profile?.storage?.toArray()?.[0]?.["@id"];
 
